e7ac956006f36966316f8fde6a3c46df081e807e
Ricky Kaare Engelharth <ren@delegate.dk>
2021-05-16 17:21:36 +0200

    Add FakeCosmos for testing connected reads and writes

---
+++ src/Atc.Cosmos/Testing/FakeCosmos.cs
@@ -0,0 +1,284 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace Atc.Cosmos.Testing
+{
+    [SuppressMessage(
+       "Design",
+       "MA0016:Prefer return collection abstraction instead of implementation",
+       Justification = "By design")]
+    [SuppressMessage(
+       "Design",
+       "CA1002:Do not expose generic lists",
+       Justification = "By design")]
+    [SuppressMessage(
+       "Usage",
+       "CA2227:Collection properties should be read only",
+       Justification = "By design")]
+    public sealed class FakeCosmos<T> :
+        ICosmosReader<T>,
+        ICosmosWriter<T>,
+        ICosmosBulkReader<T>,
+        ICosmosBulkWriter<T>
+        where T : class, ICosmosResource
+    {
+        public FakeCosmos()
+            : this(
+                  new FakeCosmosReader<T>(),
+                  new FakeCosmosWriter<T>())
+        {
+        }
+
+        public FakeCosmos(
+            FakeCosmosReader<T> reader,
+            FakeCosmosWriter<T> writer)
+        {
+            Reader = reader;
+            Writer = writer;
+            writer.Documents = reader.Documents;
+        }
+
+        public List<T> Documents
+        {
+            get => Reader.Documents;
+            set
+            {
+                Reader.Documents = value;
+                Writer.Documents = value;
+            }
+        }
+
+        public List<object> QueryResults
+        {
+            get => Reader.QueryResults;
+            set
+            {
+                Reader.QueryResults = value;
+            }
+        }
+
+        public FakeCosmosReader<T> Reader { get; }
+
+        public FakeCosmosWriter<T> Writer { get; }
+
+        Task<T?> ICosmosReader<T>.FindAsync(
+            string documentId,
+            string partitionKey,
+            CancellationToken cancellationToken)
+            => ((ICosmosReader<T>)Reader)
+                .FindAsync(
+                    documentId,
+                    partitionKey,
+                    cancellationToken);
+
+        Task<T> ICosmosReader<T>.ReadAsync(
+            string documentId,
+            string partitionKey,
+            CancellationToken cancellationToken)
+            => ((ICosmosReader<T>)Reader)
+                .ReadAsync(
+                    documentId,
+                    partitionKey,
+                    cancellationToken);
+
+        IAsyncEnumerable<T> ICosmosReader<T>.ReadAllAsync(
+            string partitionKey,
+            CancellationToken cancellationToken)
+            => ((ICosmosReader<T>)Reader)
+                .ReadAllAsync(
+                    partitionKey,
+                    cancellationToken);
+
+        IAsyncEnumerable<T> ICosmosReader<T>.QueryAsync(
+            QueryDefinition query,
+            string partitionKey,
+            CancellationToken cancellationToken)
+            => ((ICosmosReader<T>)Reader)
+                .QueryAsync(
+                    query,
+                    partitionKey,
+                    cancellationToken);
+
+        IAsyncEnumerable<TResult> ICosmosReader<T>.QueryAsync<TResult>(
+            QueryDefinition query,
+            string partitionKey,
+            CancellationToken cancellationToken)
+            => ((ICosmosReader<T>)Reader)
+                .QueryAsync<TResult>(
+                    query,
+                    partitionKey,
+                    cancellationToken);
+
+        Task<T?> ICosmosBulkReader<T>.FindAsync(
+            string documentId,
+            string partitionKey,
+            CancellationToken cancellationToken)
+            => ((ICosmosBulkReader<T>)Reader)
+                .FindAsync(
+                    documentId,
+                    partitionKey,
+                    cancellationToken);
+
+        Task<T> ICosmosBulkReader<T>.ReadAsync(
+            string documentId,
+            string partitionKey,
+            CancellationToken cancellationToken)
+            => ((ICosmosBulkReader<T>)Reader)
+                .ReadAsync(
+                    documentId,
+                    partitionKey,
+                    cancellationToken);
+
+        IAsyncEnumerable<T> ICosmosBulkReader<T>.ReadAllAsync(
+            string partitionKey,
+            CancellationToken cancellationToken)
+            => ((ICosmosBulkReader<T>)Reader)
+                .ReadAllAsync(
+                    partitionKey,
+                    cancellationToken);
+
+        IAsyncEnumerable<T> ICosmosBulkReader<T>.QueryAsync(
+            QueryDefinition query,
+            string partitionKey,
+            CancellationToken cancellationToken)
+            => ((ICosmosBulkReader<T>)Reader)
+                .QueryAsync(
+                    query,
+                    partitionKey,
+                    cancellationToken);
+
+        IAsyncEnumerable<TResult> ICosmosBulkReader<T>.QueryAsync<TResult>(
+            QueryDefinition query,
+            string partitionKey,
+            CancellationToken cancellationToken)
+            => ((ICosmosBulkReader<T>)Reader)
+                .QueryAsync<TResult>(
+                    query,
+                    partitionKey,
+                    cancellationToken);
+
+        Task<T> ICosmosWriter<T>.CreateAsync(
+            T document,
+            CancellationToken cancellationToken)
+            => ((ICosmosWriter<T>)Writer)
+                .CreateAsync(
+                    document,
+                    cancellationToken);
+
+        Task<T> ICosmosWriter<T>.WriteAsync(
+            T document,
+            CancellationToken cancellationToken)
+            => ((ICosmosWriter<T>)Writer)
+                .WriteAsync(
+                    document,
+                    cancellationToken);
+
+        Task<T> ICosmosWriter<T>.ReplaceAsync(
+            T document,
+            CancellationToken cancellationToken)
+            => ((ICosmosWriter<T>)Writer)
+                .ReplaceAsync(
+                    document,
+                    cancellationToken);
+
+        Task ICosmosWriter<T>.DeleteAsync(
+            string documentId,
+            string partitionKey,
+            CancellationToken cancellationToken)
+            => ((ICosmosWriter<T>)Writer)
+                .DeleteAsync(
+                    documentId,
+                    partitionKey,
+                    cancellationToken);
+
+        Task<T> ICosmosWriter<T>.UpdateAsync(
+            string documentId,
+            string partitionKey,
+            Func<T, Task> updateDocument,
+            int retries,
+            CancellationToken cancellationToken)
+            => ((ICosmosWriter<T>)Writer)
+                .UpdateAsync(
+                    documentId,
+                    partitionKey,
+                    updateDocument,
+                    retries,
+                    cancellationToken);
+
+        Task<T> ICosmosWriter<T>.UpdateAsync(
+            string documentId,
+            string partitionKey,
+            Action<T> updateDocument,
+            int retries,
+            CancellationToken cancellationToken)
+            => ((ICosmosWriter<T>)Writer)
+                .UpdateAsync(
+                    documentId,
+                    partitionKey,
+                    updateDocument,
+                    retries,
+                    cancellationToken);
+
+        Task<T> ICosmosWriter<T>.UpdateOrCreateAsync(
+            Func<T> getDefaultDocument,
+            Func<T, Task> updateDocument,
+            int retries,
+            CancellationToken cancellationToken)
+            => ((ICosmosWriter<T>)Writer)
+                .UpdateOrCreateAsync(
+                    getDefaultDocument,
+                    updateDocument,
+                    retries,
+                    cancellationToken);
+
+        Task<T> ICosmosWriter<T>.UpdateOrCreateAsync(
+            Func<T> getDefaultDocument,
+            Action<T> updateDocument,
+            int retries,
+            CancellationToken cancellationToken)
+            => ((ICosmosWriter<T>)Writer)
+                .UpdateOrCreateAsync(
+                    getDefaultDocument,
+                    updateDocument,
+                    retries,
+                    cancellationToken);
+
+        Task ICosmosBulkWriter<T>.CreateAsync(
+             T document,
+             CancellationToken cancellationToken)
+             => ((ICosmosBulkWriter<T>)Writer)
+                 .CreateAsync(
+                     document,
+                     cancellationToken);
+
+        Task ICosmosBulkWriter<T>.WriteAsync(
+            T document,
+            CancellationToken cancellationToken)
+            => ((ICosmosBulkWriter<T>)Writer)
+                .WriteAsync(
+                    document,
+                    cancellationToken);
+
+        Task ICosmosBulkWriter<T>.ReplaceAsync(
+            T document,
+            CancellationToken cancellationToken)
+            => ((ICosmosBulkWriter<T>)Writer)
+                .ReplaceAsync(
+                    document,
+                    cancellationToken);
+
+        Task ICosmosBulkWriter<T>.DeleteAsync(
+            string documentId,
+            string partitionKey,
+            CancellationToken cancellationToken)
+            => ((ICosmosBulkWriter<T>)Writer)
+                .DeleteAsync(
+                    documentId,
+                    partitionKey,
+                    cancellationToken);
+    }
+}
---
+++ test/Atc.Cosmos.Tests/Testng/FakeCosmosTests.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Threading.Tasks;
+using Atc.Cosmos.Testing;
+using Atc.Test;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Microsoft.Azure.Cosmos;
+using NSubstitute;
+using Xunit;
+
+namespace Atc.Cosmos.Tests.Testng
+{
+    public class FakeCosmosTests
+    {
+        [Theory, AutoNSubstituteData]
+        public void Should_Have_Reader(
+            FakeCosmos<Record> sut)
+        {
+            sut.Reader.Should().NotBeNull();
+            sut.Reader.Documents.Should().BeSameAs(sut.Documents);
+            sut.Reader.QueryResults.Should().BeSameAs(sut.QueryResults);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void Should_Have_Writer(
+            FakeCosmos<Record> sut)
+        {
+            sut.Writer.Should().NotBeNull();
+            sut.Writer.Documents.Should().BeSameAs(sut.Documents);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void Should_Be_Able_To_Inject_As_Frozen_CosmosReader(
+            [Frozen(Matching.ImplementedInterfaces)]
+            FakeCosmos<Record> sut,
+            TestCosmosService<Record> service)
+        {
+            service.Reader.Should().BeSameAs(sut);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void Should_Be_Able_To_Inject_As_Frozen_CosmosBulkReader(
+            [Frozen(Matching.ImplementedInterfaces)]
+            FakeCosmos<Record> sut,
+            TestCosmosService<Record> service)
+        {
+            service.BulkReader.Should().BeSameAs(sut);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void Should_Be_Able_To_Inject_As_Frozen_CosmosWriter(
+            [Frozen(Matching.ImplementedInterfaces)]
+            FakeCosmos<Record> sut,
+            TestCosmosService<Record> service)
+        {
+            service.Writer.Should().BeSameAs(sut);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void Should_Be_Able_To_Inject_As_Frozen_CosmosBulkWriter(
+            [Frozen(Matching.ImplementedInterfaces)]
+            FakeCosmos<Record> sut,
+            TestCosmosService<Record> service)
+        {
+            service.BulkWriter.Should().BeSameAs(sut);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void Should_Forward_CosmosReader_Calls(
+            [Frozen, Substitute] FakeCosmosReader<Record> reader,
+            [Greedy] FakeCosmos<Record> sut,
+            string documentId,
+            string partitionKey,
+            QueryDefinition query)
+        {
+            var sutReader = (ICosmosReader<Record>)sut;
+            _ = sutReader.FindAsync(documentId, partitionKey);
+            _ = sutReader.ReadAsync(documentId, partitionKey);
+            sutReader.ReadAllAsync(partitionKey);
+            sutReader.QueryAsync(query, partitionKey);
+            sutReader.QueryAsync<RecordAggregate>(query, partitionKey);
+
+            _ = reader.Received(1).FindAsync(documentId, partitionKey);
+            _ = reader.Received(1).ReadAsync(documentId, partitionKey);
+            reader.Received(1).ReadAllAsync(partitionKey);
+            reader.Received(1).QueryAsync(query, partitionKey);
+            reader.Received(1).QueryAsync<RecordAggregate>(query, partitionKey);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void Should_Forward_CosmosWriter_Calls(
+            [Frozen, Substitute] FakeCosmosWriter<Record> writer,
+            [Greedy] FakeCosmos<Record> sut,
+            Record document,
+            string documentId,
+            string partitionKey,
+            Action<Record> updateDocument,
+            Func<Record, Task> updateDocumentAsync,
+            Func<Record> getDefaultDocument)
+        {
+            var sutWriter = (ICosmosWriter<Record>)sut;
+            _ = sutWriter.CreateAsync(document);
+            _ = sutWriter.WriteAsync(document);
+            _ = sutWriter.ReplaceAsync(document);
+            _ = sutWriter.DeleteAsync(documentId, partitionKey);
+            _ = sutWriter.UpdateAsync(documentId, partitionKey, updateDocument);
+            _ = sutWriter.UpdateAsync(documentId, partitionKey, updateDocumentAsync);
+            _ = sutWriter.UpdateOrCreateAsync(getDefaultDocument, updateDocument);
+            _ = sutWriter.UpdateOrCreateAsync(getDefaultDocument, updateDocumentAsync);
+
+            _ = writer.Received(1).CreateAsync(document);
+            _ = writer.Received(1).WriteAsync(document);
+            _ = writer.Received(1).ReplaceAsync(document);
+            _ = writer.Received(1).DeleteAsync(documentId, partitionKey);
+            _ = writer.Received(1).UpdateAsync(documentId, partitionKey, updateDocument);
+            _ = writer.Received(1).UpdateAsync(documentId, partitionKey, updateDocumentAsync);
+            _ = writer.Received(1).UpdateOrCreateAsync(getDefaultDocument, updateDocument);
+            _ = writer.Received(1).UpdateOrCreateAsync(getDefaultDocument, updateDocumentAsync);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void Should_Forward_CosmosBulkReader_Calls(
+            [Frozen, Substitute] FakeCosmosReader<Record> reader,
+            [Greedy] FakeCosmos<Record> sut,
+            string documentId,
+            string partitionKey,
+            QueryDefinition query)
+        {
+            var sutReader = (ICosmosBulkReader<Record>)sut;
+            _ = sutReader.FindAsync(documentId, partitionKey);
+            _ = sutReader.ReadAsync(documentId, partitionKey);
+            sutReader.ReadAllAsync(partitionKey);
+            sutReader.QueryAsync(query, partitionKey);
+            sutReader.QueryAsync<RecordAggregate>(query, partitionKey);
+
+            _ = reader.Received(1).FindAsync(documentId, partitionKey);
+            _ = reader.Received(1).ReadAsync(documentId, partitionKey);
+            reader.Received(1).ReadAllAsync(partitionKey);
+            reader.Received(1).QueryAsync(query, partitionKey);
+            reader.Received(1).QueryAsync<RecordAggregate>(query, partitionKey);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void Should_Forward_CosmosBulkWriter_Calls(
+            [Frozen, Substitute] FakeCosmosWriter<Record> writer,
+            [Greedy] FakeCosmos<Record> sut,
+            Record document,
+            string documentId,
+            string partitionKey)
+        {
+            var sutWriter = (ICosmosBulkWriter<Record>)sut;
+            _ = sutWriter.CreateAsync(document);
+            _ = sutWriter.WriteAsync(document);
+            _ = sutWriter.ReplaceAsync(document);
+            _ = sutWriter.DeleteAsync(documentId, partitionKey);
+
+            _ = writer.Received(1).CreateAsync(document);
+            _ = writer.Received(1).WriteAsync(document);
+            _ = writer.Received(1).ReplaceAsync(document);
+            _ = writer.Received(1).DeleteAsync(documentId, partitionKey);
+        }
+    }
+}
