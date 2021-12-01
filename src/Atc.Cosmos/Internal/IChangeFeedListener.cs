1fde8ec061031487f5bfa8071294f0b77d5ae924
Ricky Kaare Engelharth <ren@delegate.dk>
2021-12-01 20:49:29 +0100

    Move IChangeFeedListener<T> out of internal namespace

--- src/Atc.Cosmos/Internal/IChangeFeedListener.cs
+++ src/Atc.Cosmos/Internal/IChangeFeedListener.cs
@@ -3,10 +3,24 @@ using System.Threading.Tasks;
 
 namespace Atc.Cosmos.Internal
 {
+    /// <summary>
+    /// Represents a change feed listener and can be used
+    /// to start and stop the listening for changes.
+    /// </summary>
     public interface IChangeFeedListener
     {
+        /// <summary>
+        /// Start listening for changes.
+        /// </summary>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> used.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         Task StartAsync(CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Stop listening for changes.
+        /// </summary>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> used.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         Task StopAsync(CancellationToken cancellationToken);
     }
 }
--- src/Atc.Cosmos/Internal/IChangeFeedListener{T}.cs
+++
@@ -1,7 +0,0 @@
-namespace Atc.Cosmos.Internal
-{
-    public interface IChangeFeedListener<T> : IChangeFeedListener
-        where T : class, ICosmosResource
-    {
-    }
-}
