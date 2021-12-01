2b43d48b8c485aef68c22e249011451cf06f4726
Ricky Kaare Engelharth <ren@delegate.dk>
2021-12-01 20:49:46 +0100

    Improve code doc

--- src/Atc.Cosmos/Internal/ICosmosContainerProvider.cs
+++ src/Atc.Cosmos/Internal/ICosmosContainerProvider.cs
@@ -1,14 +1,46 @@
-﻿using Microsoft.Azure.Cosmos;
+using System;
+using Microsoft.Azure.Cosmos;
 
 namespace Atc.Cosmos.Internal
 {
     /// <summary>
-    /// Factory is responsible for providing access to Cosmos DB containers.
+    /// Represents a provider for cosmos <see cref="Container"/> instances.
     /// </summary>
     public interface ICosmosContainerProvider
     {
+        /// <summary>
+        /// Get the configured container for the specified <see cref="ICosmosResource"/> type.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="ICosmosResource"/>.</typeparam>
+        /// <param name="allowBulk">
+        /// Boolean indicating if the container should
+        /// be configured for bulk operations. Default is false.
+        /// </param>
+        /// <returns>A cosmos <see cref="Container"/>.</returns>
         Container GetContainer<T>(bool allowBulk = false);
 
+        /// <summary>
+        /// Get the configured container for the specified <see cref="ICosmosResource"/> type.
+        /// </summary>
+        /// <param name="resourceType">
+        /// The <see cref="Type"/> of the <see cref="ICosmosResource"/>.
+        /// </param>
+        /// <param name="allowBulk">
+        /// Boolean indicating if the container should
+        /// be configured for bulk operations. Default is false.
+        /// </param>
+        /// <returns>A cosmos <see cref="Container"/>.</returns>
+        Container GetContainer(Type resourceType, bool allowBulk = false);
+
+        /// <summary>
+        /// Get the container with a specified name.
+        /// </summary>
+        /// <param name="name">The name of the container.</param>
+        /// <param name="allowBulk">
+        /// Boolean indicating if the container should
+        /// be configured for bulk operations. Default is false.
+        /// </param>
+        /// <returns>A cosmos <see cref="Container"/>.</returns>
         Container GetContainer(string name, bool allowBulk = false);
     }
 }
--- src/Atc.Cosmos/Internal/ICosmosInitializer.cs
+++ src/Atc.Cosmos/Internal/ICosmosInitializer.cs
@@ -1,10 +1,20 @@
-﻿using System.Threading;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Atc.Cosmos.Internal
 {
+    /// <summary>
+    /// Represents the initializer responsible for executing
+    /// the configured <see cref="ICosmosContainerInitializer"/>s.
+    /// </summary>
     public interface ICosmosInitializer
     {
+        /// <summary>
+        /// Run the initialization that will execute the
+        /// configured <see cref="ICosmosContainerInitializer"/>s.
+        /// </summary>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> used.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         Task InitializeAsync(CancellationToken cancellationToken);
     }
 }
