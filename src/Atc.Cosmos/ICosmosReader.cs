072f6c2695bf4b8552d51e7921d5e0e6f57c4ffd
Christian Helle <christian.helle@outlook.com>
2022-11-16 08:52:19 +0100

    Add comment suggestion NOT to read data across partitions

--- src/Atc.Cosmos/ICosmosReader.cs
+++ src/Atc.Cosmos/ICosmosReader.cs
@@ -61,6 +61,9 @@ namespace Atc.Cosmos
         /// <summary>
         /// Reads all the specified <typeparamref name="T"/> resource from the configured
         /// Cosmos collection.
+        ///
+        /// It is NOT recommended to read data across partitions.
+        /// This method is provided for convenience for scenarios where ALL data needs to processed or re-processed.
         /// </summary>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> used.</param>
         /// <returns>An <see cref="IAsyncEnumerable&lt;T&gt;"/> over all the <typeparamref name="T"/> resources.</returns>
