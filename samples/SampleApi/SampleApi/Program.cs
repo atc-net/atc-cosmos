39d89f22edd81755e1b5fb46ffda7469fc39cd64
Christian Helle <christian.helle@outlook.com>
2023-02-11 22:36:06 +0100

    Revert Refactor endpoint definitions into its own file

--- samples/SampleApi/SampleApi/Program.cs
+++ samples/SampleApi/SampleApi/Program.cs
@@ -1,3 +1,5 @@
+using Atc.Cosmos;
+using Microsoft.AspNetCore.Mvc;
 using SampleApi;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,8 +8,52 @@ builder.Services.AddSwaggerGen();
 builder.Services.ConfigureCosmosDb();
 
 var app = builder.Build();
+app.MapGet(
+    "/foo",
+    (
+        ICosmosReader<FooResource> reader,
+        CancellationToken cancellationToken) =>
+            reader
+                .ReadAllAsync(FooResource.PartitionKey, cancellationToken)
+                .ToBlockingEnumerable(cancellationToken)
+                .Select(c => c.Bar))
+    .WithName("ListFoo")
+    .WithOpenApi();
+
+app.MapGet(
+    "/foo/{id}",
+    async (
+        ICosmosReader<FooResource> reader,
+        string id,
+        CancellationToken cancellationToken) =>
+        {
+            var foo = await reader.FindAsync(id, FooResource.PartitionKey, cancellationToken);
+            return foo is not null ? Results.Ok(foo.Bar) : Results.NotFound(id);
+        })
+    .WithName("GetFoo")
+    .WithOpenApi();
+
+app.MapPost(
+    "/foo",
+    async (
+        ICosmosWriter<FooResource> writer,
+        [FromBody] Dictionary<string, object> data,
+        CancellationToken cancellationToken) =>
+        {
+            var id = Guid.NewGuid().ToString();
+            await writer.CreateAsync(
+                new FooResource
+                {
+                    Id = id,
+                    Bar = data,
+                },
+                cancellationToken);
+            return Results.CreatedAtRoute("GetFoo", new { id });
+        })
+    .WithName("PostFoo")
+    .WithOpenApi();
+
 app.UseHttpsRedirection();
 app.UseSwaggerUI();
 app.UseSwagger();
-app.ConfigureEndpoints();
 app.Run();
