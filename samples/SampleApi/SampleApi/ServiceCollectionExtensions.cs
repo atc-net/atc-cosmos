997c2a8de94046c10b474793313985b6f96e3f0a
Christian Helle <christian.helle@outlook.com>
2023-02-11 22:35:50 +0100

    Remove unnecessary call to AddOptions<CosmosOptions>()

--- samples/SampleApi/SampleApi/ServiceCollectionExtensions.cs
+++ samples/SampleApi/SampleApi/ServiceCollectionExtensions.cs
@@ -1,13 +1,9 @@
-using Atc.Cosmos;
-
 namespace SampleApi;
 
 public static class ServiceCollectionExtensions
 {
     public static void ConfigureCosmosDb(this IServiceCollection services)
     {
-        // Configure Atc.Cosmos
-        services.AddOptions<CosmosOptions>();
         services.ConfigureOptions<ConfigureCosmosOptions>();
         services.ConfigureCosmos(
             cosmosBuilder =>
