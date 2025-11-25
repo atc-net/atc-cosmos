0597dfbd8f815994511a542c47391be55b46433a
Andreas Gehrke <ange@clever.dk>
2025-11-25 09:31:04 +0100

    Fix AutoFixture on CosmosOptions

---
+++ test/Atc.Cosmos.Tests/CosmosOptionsCustomization.cs
@@ -0,0 +1,26 @@
+﻿using Atc.Test.Customizations;
+using AutoFixture;
+
+namespace Atc.Cosmos.Tests;
+
+/// <summary>
+/// Customize CosmosOptions to allow AutoFixture to create it.
+/// </summary>
+/// <remarks>
+/// This class is "magically" instantiated via Atc.Test.AutoNSubstituteDataAttribute
+/// </remarks>
+[AutoRegister]
+public class CosmosOptionsCustomization : ICustomization
+{
+    public void Customize(IFixture fixture)
+    {
+        fixture.Customize<CosmosOptions>(composer =>
+            composer.FromFactory(() => new CosmosOptions
+                {
+                    AccountEndpoint = "SomeEndpoint",
+                    AccountKey = "SomeAccountKey",
+                    DatabaseName = "SomeDatabase",
+                })
+                .OmitAutoProperties());
+    }
+}
